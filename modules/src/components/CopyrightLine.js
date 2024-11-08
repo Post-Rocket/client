07751d92c0e8a656d1cecc2bc65c308ac955914b
William Brendel <brendel.william@gmail.com>
2024-11-08 12:22:16 -0800

    first commit

--- modules/src/components/CopyrightLine.js
+++ modules/src/components/CopyrightLine.js
@@ -44,7 +44,7 @@ export class CopyrightLine extends HTMLElement {
       this.#anchor.setAttribute("href", href)
     );
     target && this.#anchor.setAttribute("target", target);
-    this.setAttribute("title", `Copyright ${text.replace(/(\s+|)(©|Copyright)/gi, "").trim()}`);
+    this.setAttribute("title", `Copyright ${text.replace(/(\s+|)(©|Copyright)/gi, "").trim()}. All rights reserved`);
     this.#anchor.innerHTML = text;
   }
 }
