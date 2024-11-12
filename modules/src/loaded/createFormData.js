cabd9e06fc534e1244de939be5ef5052716883de
William Brendel <brendel.william@gmail.com>
2024-11-12 14:36:34 -0800

    first commit

--- modules/src/loaded/createFormData.js
+++ modules/src/loaded/createFormData.js
@@ -18,9 +18,20 @@ export const createFormData = (input, defaultOutput = {}) => {
 
   // If input is an event instead of a form.
   input instanceof HTMLFormElement || (isSubmitEvent(input) && (
-    submitter = input.submitter,
+    submitter = input.submitter || (
+      input.relatedTarget && (
+        (input.relatedTarget.type || input.relatedTarget.getAttribute("type")) === "submit"
+        || input.relatedTarget.nodeName.toLowerCase() === "button"
+      ) && input.relatedTarget
+    ),
     input = input.target
   ));
+
+  submitter && (submitter = ({
+    name: submitter.name || submitter.getAttribute("name"),
+    value: submitter.value || submitter.getAttribute("value")
+  }));
+  submitter.value || (submitter.value = !!submitter.name);
 
   // If we can't capture the form element.
   if (!(input instanceof HTMLFormElement)) return defaultOutput;
@@ -28,7 +39,7 @@ export const createFormData = (input, defaultOutput = {}) => {
   // Format data, ignoring empty values.
   const output = {};
   input = new FormData(input);
-  submitter && submitter.name && input.append(submitter.name, submitter.value || true);
+  submitter && submitter.name && input.append(submitter.name, submitter.value);
   for (const pair of input) {
     const [key, value] = pair;
     (typeof value === 'number' || typeof value === 'boolean' || value) && (
