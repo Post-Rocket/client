2fe90c87685a61de59cec6e14921d9c9e26e39c8
William Brendel <brendel.william@gmail.com>
2024-11-12 14:26:03 -0800

    first commit

--- modules/src/loaded/createFormData.js
+++ modules/src/loaded/createFormData.js
@@ -14,9 +14,13 @@ const isSubmitEvent = event => {
 // Create data from a form.
 export const createFormData = (input, defaultOutput = {}) => {
   if (!input || typeof input !== 'object') return defaultOutput;
+  let submitter;
 
   // If input is an event instead of a form.
-  input instanceof HTMLFormElement || (isSubmitEvent(input) && (input = input.target));
+  input instanceof HTMLFormElement || (isSubmitEvent(input) && (
+    submitter = input.submitter,
+    input = input.target
+  ));
 
   // If we can't capture the form element.
   if (!(input instanceof HTMLFormElement)) return defaultOutput;
@@ -24,6 +28,7 @@ export const createFormData = (input, defaultOutput = {}) => {
   // Format data, ignoring empty values.
   const output = {};
   input = new FormData(input);
+  submitter && submitter.name && input.append(submitter.name, submitter.value || true);
   for (const pair of input) {
     const [key, value] = pair;
     (typeof value === 'number' || typeof value === 'boolean' || value) && (
