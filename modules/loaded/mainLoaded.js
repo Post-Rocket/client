f50b752c6123967cc67539dfbbb87d0e516c4d7b
William Brendel <brendel.william@gmail.com>
2024-11-25 11:46:49 -0800

    YouTube preload

--- modules/loaded/mainLoaded.js
+++ modules/loaded/mainLoaded.js
@@ -531,7 +531,7 @@ intro && (
   setTimeout(() => {
     unhide();
     addPulsingShaking();
-  }, !document.cookie.includes("__Host-postrocket_index_intro=1") & 13000 || 0)
+  }, !document.cookie.includes("__Host-postrocket_index_intro=1") && 13000 || 100)
 ) || (
   unhide(),
   addPulsingShaking()
