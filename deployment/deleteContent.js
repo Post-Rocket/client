ae9fd2241ed2ee870f18ee1e1f29ebff3bd8898e
William Brendel <brendel.william@gmail.com>
2024-12-23 11:37:55 -0800

    deploy

--- deployment/deleteContent.js
+++ deployment/deleteContent.js
@@ -7,7 +7,7 @@ const deleteContent = async (...content) => new Promise(async (resolve, reject) 
       Bucket,
       Objects: content.flat(Infinity).map(f => f && (
         typeof f === "object" && f.Key && {...f, Key: f.Key.replace(/^\.\//, "")} || {
-          Key: `${f}`.Key.replace(/^\.\//, "")
+          Key: `${f}`.replace(/^\.\//, "")
         })
       ).filter(f => f)
     })) || [];
